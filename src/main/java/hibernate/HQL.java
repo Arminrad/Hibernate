746744e60b86937085cad1511c7143c3837a4f9c
Siavash <siavosh.arminrad@gmail.com>
2022-06-13 14:02:23 +0430

    SELECT two fields by hql

--- src/main/java/hibernate/HQL.java
+++ src/main/java/hibernate/HQL.java
@@ -2,6 +2,7 @@ package hibernate;
 
 import org.hibernate.query.Query;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class HQL {
@@ -23,10 +24,25 @@ public class HQL {
 
 
         // SELECT * FROM
-        String hql = "FROM hibernate.Contact";
+/*        String hql = "FROM hibernate.Contact";
         Query<Contact> query = session.createQuery(hql, Contact.class);
         List<Contact> contacts = query.list();
         contacts
-                .forEach(System.out::println);
+                .forEach(System.out::println);*/
+
+/*        String hql = "SELECT c.firstName, c.lastName FROM hibernate.Contact c";
+        List<Object[]> query = session.createQuery(hql).list();
+        query.forEach(objects -> System.out.println(Arrays.toString(objects)));*/
+
+/*        String hql = "FROM hibernate.Contact c WHERE c.firstName = :firstName"; // to set a named parameter we use : .for unnamed parameter we use ?
+        var query = session.createQuery(hql, Contact.class);
+        query.setParameter("firstName", "siavosh");
+        query.getResultStream().forEach(System.out::println);*/
+
+        /*String hql = "FROM hibernate.Contact c WHERE c.firstName = :firstName AND c.phoneNumber = :phoneNumber";
+        var query = session.createQuery(hql, Contact.class);
+        query.setParameter("firstName", "siavosh");
+        query.setParameter("phoneNumber", "09304714334");
+        query.getResultStream().forEach(System.out::println);*/
     }
 }
